ae57ba5d76c4a6026ffd02dd92798a45abc40620
MSI\stend <stendb29@gmail.com>
2021-05-05 13:12:03 +0200

    fixed cam

--- pocketMonster/Assets/Scripts/Player/PlayerCam.cs
+++ pocketMonster/Assets/Scripts/Player/PlayerCam.cs
@@ -9,7 +9,9 @@ public class PlayerCam : MonoBehaviour
     [SerializeField]
     private float distanceXZ = 0, distanceY = 0, smoothPos = 0, smoothRot = 0, lookDownFactor = 0;
 
-    void FixedUpdate()
+    private List<GameObject> allObjectsInFrontOfCam = new List<GameObject>();
+
+    void LateUpdate()
     {
         if (!player)
         {
@@ -21,23 +23,28 @@ public class PlayerCam : MonoBehaviour
             return;
         }
 
-        Vector3 newRotation = Vector3.zero;
-        newRotation += transform.eulerAngles;
-        newRotation += player.eulerAngles;
-        newRotation /= 2;
-
-        if (player.eulerAngles.y - transform.localEulerAngles.y < -300)
-        {
-            newRotation.y = 360;
-        }
-        else if (player.eulerAngles.y - transform.localEulerAngles.y > 300)
-        {
-            newRotation.y = 1;
-        }
-
-        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(lookDownFactor, newRotation.y, 0), Time.deltaTime * smoothRot);
+        Vector3 lookRotation = Quaternion.LookRotation(player.forward, Vector3.up).eulerAngles;
+        Vector3 newRotation = new Vector3(lookDownFactor, lookRotation.y, 0);
+        Quaternion newRotationInQuaternion = Quaternion.Euler(newRotation);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotationInQuaternion, Time.deltaTime * smoothRot);
 
         transform.position = Vector3.Slerp(transform.position, new Vector3(player.transform.position.x + -player.transform.forward.x * distanceXZ,
             player.transform.position.y + distanceY, player.transform.position.z + -player.transform.forward.z * distanceXZ), Time.deltaTime * smoothPos);
+
+        for (int i = allObjectsInFrontOfCam.Count - 1; i >= 0; i--)
+        {
+            allObjectsInFrontOfCam[i].GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+            allObjectsInFrontOfCam.RemoveAt(i);
+        }
+
+        float dist = Vector3.Distance(transform.position, player.transform.position) / 1.2f;
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, dist))
+        {
+            if (hit.transform.tag == "EnvironmentObject")
+            {
+                allObjectsInFrontOfCam.Add(hit.transform.gameObject);
+                hit.transform.GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+            }
+        }
     }
 }
